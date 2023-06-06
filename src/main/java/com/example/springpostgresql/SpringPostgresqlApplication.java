32d767e49c28df0388ee2fa9c8c9eae59de7a543
mustafaulvi <ulvimustafa@gmail.com>
2023-06-06 23:25:29 +0200

    set docker postgres

--- src/main/java/com/example/springpostgresql/SpringPostgresqlApplication.java
+++ src/main/java/com/example/springpostgresql/SpringPostgresqlApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringPostgresqlApplication {
+public class
+SpringPostgresqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPostgresqlApplication.class, args);
