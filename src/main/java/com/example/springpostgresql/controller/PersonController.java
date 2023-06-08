ea0fc9859df1ded02fdc0097d988873b26c2fe0e
mustafaulvi <ulvimustafa@gmail.com>
2023-06-08 08:13:43 +0200

    Controller classes have been created.

---
+++ src/main/java/com/example/springpostgresql/controller/PersonController.java
@@ -0,0 +1,26 @@
+package com.example.springpostgresql.controller;
+
+import com.example.springpostgresql.dto.PersonDto;
+import com.example.springpostgresql.services.PersonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+@RequiredArgsConstructor
+public class PersonController {
+    private PersonService personService;
+
+    @GetMapping
+    public ResponseEntity<List<PersonDto>> getAllPerson (){
+        return ResponseEntity.ok(personService.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<PersonDto> savePerson(@RequestBody PersonDto personDto) {
+        return ResponseEntity.ok(personService.save(personDto));
+    }
+}
